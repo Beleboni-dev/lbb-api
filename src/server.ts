8957294b97960b4a84ab25d951bb3cf89234e009
Lucas Beleboni <beleboni.dev@gmail.com>
2023-07-15 16:03:53 -0300

    initial commit

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 /// <reference path="./types/express.d.ts" />
 
 import express, { Request, Response } from "express";
-import mongoose from "mongoose";
 import { userRoutes } from "./routes/userRoutes";
 import cors from "cors"
+import connectDB from "./db";
 const app = express();
+
 const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
 
 app.use(express.json());
 app.use(cors())
@@ -14,14 +18,4 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.use("/", userRoutes);
-
-mongoose
-  .connect("mongodb://localhost:27017/lbb")
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Servidor API LBB rodando em http://localhost:${PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.error("Erro ao conectar ao banco de dados:", error);
-  });
+connectDB()
