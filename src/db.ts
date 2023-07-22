d7dd28c2eb40b7c2cb7279abbf3695a9aad4eaa1
Lucas Beleboni <beleboni.dev@gmail.com>
2023-07-22 19:36:40 -0300

    postgres connect url

--- src/db.ts
+++ src/db.ts
@@ -1,11 +1,7 @@
 import { Pool } from "pg";
 
 const pool = new Pool({
-  user: "alpyroot",
-  host: "dpg-ciu3cbtiuiedpv6vdtgg-a", // Endereço do banco de dados no Render
-  database: "lbbdatabase", // Nome do banco de dados
-  password: "AtPf9bnRuR84zWC09QI0cY9wDFDzuNKM", // Substitua pela sua senha do banco de dados
-  port: 5432, // Porta padrão do PostgreSQL
+  connectionString: "postgres://alpyroot:AtPf9bnRuR84zWC09QI0cY9wDFDzuNKM@dpg-ciu3cbtiuiedpv6vdtgg-a/lbbdatabase"
 });
 
 export default pool;
